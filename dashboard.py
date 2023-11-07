fc445b65e2858b88dee31abfc0e306b8948a6ad8
Ryoru11 <121512208+Ryoru11@users.noreply.github.com>
2023-11-08 00:08:44 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,27 @@
-from dash import Dash
+import dash
 import dash_html_components as html
 import dash_core_components as dcc
+import conception_carte
 
+my_app = dash.Dash("Projet Datascience")
 
-my_app = Dash("Projet Datascience")
-
-my_app.layout = html.div([
-    html.h1('Project Datacience'),
-    html.TextInput(label ='Year', value ='2019', id = 'my-input'),
+my_app.layout = html.Div([
+    html.H1('Project Datacience'),
+    dcc.Input(
+            id="input_{}".format(_),
+            type=_,
+            placeholder="input type {}".format(_),
+        )
+        for _ in ALLOWED_TYPES #A changer avec le nombre d'année unique
+    
+    
+    #Prendre le Input et le mettre dans conception_carte.py
+    html.Iframe(id = 'Map', src = 'map.html', style={"height": "1067px", "width": "100%"}),
     html.Plotlygraph
-])
+])+ [html.Div(id="out-all-types")]
 
-my_app.server.run(debug = True)
+@dash.callback(
+    dcc.Output("out-all-types", "children"),
+    [dcc.Input("input_{}".format(_), "value") for _ in ALLOWED_TYPES])
+
+my_app.Dash.server.run(debug = True)
