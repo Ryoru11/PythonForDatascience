b29b9b7d69ad3c9748111a6df5a7fae4e611208d
Ryoru11 <121512208+Ryoru11@users.noreply.github.com>
2023-11-13 22:35:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+################################
+#Code réalisé par Alexandre NGUYEN pour le projet de Python et Datascience 2023
+#Mis à jour le 12/11
+#Le fichier 'main.py' permet d'initier le projet sur la console
+
+
+################################
+
+
 import dashboard
 
 def main():
+    #Lancement du fichier 'dashboard'
     dashboard.run()
     
 
